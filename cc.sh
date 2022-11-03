17fe53d1b320d3e887056c9226aa3771be896170
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-11-03 21:25:18 +0100

    Clean pycache recursively

---
+++ cc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Recursively remove the Cython compiled
+# __pycache__ directories from root ".",
+# use with care! Invoking rm -rf on all
+# directories with name *pycache*.
+function traverse() {
+  for file in "$1"/*
+  do
+    if [ ! -d "${file}" ] ; then
+      continue
+    else
+      if [[ "${file}" == *"pycache"* ]] ; then
+        rm -rf "${file}"
+      else
+        traverse "${file}"
+      fi
+    fi
+  done
+}
+
+traverse "."
+
