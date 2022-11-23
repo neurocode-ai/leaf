cfd762607d16d3903c8eece65eefbb2ce090da51
Eli Vance <wilhelmagren98@gmail.com>
2022-11-23 17:40:52 +0100

    Implement Exponent, Logaritm, ReLU unary operations (#18)

---
+++ leaf/functions/_unary_ops.py
@@ -0,0 +1,57 @@
+"""
+MIT License
+
+Copyright (c) 2022 Neurocode
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2022-11-23
+Last updated: 2022-11-23
+"""
+
+import numpy as np
+from .function import Function
+
+class Exp(Function):
+    def forward(self, x):
+        result = np.exp(x.clip(-50, 50))
+        self.save_for_backward(result)
+        return result
+    
+    def backward(self, grad):
+        exp, = self.saved_tensors
+        return grad * exp
+
+class Log(Function):
+    def forward(self, x):
+        self.save_for_backward(x)
+        return np.log(x)
+    
+    def backward(self, grad):
+        x, = self.saved_tensors
+        return grad * 1.0 / x
+
+class ReLU(Function):
+    def forward(self, x):
+        self.save_for_backward(x)
+        return np.maximum(x, 0.0)
+
+    def backward(self, grad):
+        x, = self.saved_tensors
+        return grad * (x >= 0.0)
