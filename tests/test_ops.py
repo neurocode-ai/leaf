1884076c7568936d52b74adeae01f5dc25bbe71c
Eli Vance <wilhelmagren98@gmail.com>
2022-11-23 21:37:29 +0100

    Rewrite ops testing to compare to PyTorch speed, forward and backward (#22)

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -22,23 +22,94 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 File created: 2022-11-05
-Last edited:  2022-11-05
+Last edited:  2022-11-23
 """
+
 import unittest
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import timeit
+import time
+import leaf
+from datetime import datetime
 from leaf import Tensor
+from functools import partial
+
+np.random.seed(1)
+torch.manual_seed(1)
+
+strformat = '%Y-%m-%d %H:%M:%S'
+def _info(s):
+    sys.stdout.write(f'[{datetime.now().strftime(strformat)}] {s}')
+    sys.stdout.flush()
+
+def _test_op(shapes, torch_func, leaf_func, name, timeits=10):
+    _info(f'testing {name} with shapes {shapes}, torch/leaf \n')
+    torch_t = [torch.tensor(np.random.random(size=shape), requires_grad=True) for shape in shapes]
+    leaf_t = [Tensor(t.detach().numpy(), requires_grad=True) for t in torch_t]
+
+    torch_out = torch_func(*torch_t)
+    leaf_out = leaf_func(*leaf_t)
+
+    _listout_t = isinstance(torch_out, (tuple, list))
+    _listout_l = isinstance(leaf_out, (tuple, list))
+
+    if isinstance(torch_out, torch.Tensor):
+        torch_out = [torch_out]
+    if isinstance(leaf_out, Tensor):
+        leaf_out = [leaf_out]
+    
+    for tt, lt in zip(torch_out, leaf_out):
+        np.testing.assert_allclose(
+            tt.detach().numpy(),
+            lt.data,
+            atol=1e-6,
+            rtol=1e-3,
+        )
+        tt.mean().backward()
+        lt.mean().backward()
+
+    for tt, lt in zip(torch_t, leaf_t):
+        np.testing.assert_allclose(
+            tt.detach().numpy(),
+            lt.data,
+            atol=1e-6,
+            rtol=1e-3,
+        )
+    
+    f_torch_ms = timeit.Timer(partial(
+        torch_func,
+        *torch_t,
+    )).timeit(timeits) * 1000.0 / timeits
+    f_leaf_ms = timeit.Timer(partial(
+        leaf_func,
+        *leaf_t
+    )).timeit(timeits) * 1000.0 / timeits
+    
+    b_torch_ms = timeit.Timer(partial(
+        lambda f, t, b: f(*t)[0].mean().backward() if b else f(*t).mean().backward(),
+        torch_fun,
+        torch_t,
+        _listout_t,
+    )).timeit(timeits) * 1000.0 / timeits
+    b_leaf_ms = timeit.Timer(partial(
+        lambda f, t, b: f(*t)[0].mean().backward() if b else f(*t).mean().backward(),
+        leaf_func,
+        leaf_f,
+        _listout_l
+    )).timeit(timeits) * 1000.0 / timeits
+
+    _info(
+        f'forward: {f_torch_ms:.3f} ms / {f_leaf_ms:.3f} ms ' \
+        f'backward: {b_torch_ms:.3f} / {b_leaf_ms:.3f} ms\n'
+    )
 
 class TestOps(unittest.TestCase):
     def test_add1(self):
-        t1 = Tensor.ones(2, 3, 4)
-        t2 = Tensor.ones(2, 3, 4)
-        t3 = t1.add(t2)
-
-        assert t3.shape == (2, 3, 4)
+        _test_op([(100, 100), (100, 100)], lambda x, y: x + y, Tensor.add, 'add')
 
     def test_sub1(self):
-        t1 = Tensor.ones(2, 3, 4)
-        t2 = Tensor.ones(2, 3, 4)
-        t3 = t1.sub(t2)
-
-        assert t3.shape == (2, 3, 4)
-        assert (t3.data == Tensor.zeros(2, 3, 4).data).all()
+        _test_op([(100, 100), (100, 100)], lambda x, y: x - y, Tensor.sub, 'sub')
