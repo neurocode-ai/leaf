4e9f6c8682d6faa00233cd938bd828c62ee14862
willeagren <wilhelmagren98@gmail.com>
2023-01-13 16:47:03 +0000

    Fix header license notice formatting everywhere

---
+++ leaf/functions/__init__.py
@@ -0,0 +1,2 @@
+# This file should be empty. It only exists such that the importlib module finds this submodule 
+# and can properly add the function names to the namespace of the core leaf module.
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,29 +1,29 @@
-"""
-MIT License
-
-Copyright (c) 2022 Neurocode
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-File created: 2022-11-05
-Last edited:  2022-11-25
-"""
+#
+# MIT License
+#
+# Copyright (c) 2022 Neurocode
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# File created: 2022-11-05
+# Last updated: 2023-01-13
+#
 
 import unittest
 import sys
@@ -85,6 +85,7 @@ def _test_op(shapes, torch_func, leaf_func, name, timeits=10):
         torch_func,
         *torch_t,
     )).timeit(timeits) * 1000.0 / timeits
+
     f_leaf_ms = timeit.Timer(partial(
         leaf_func,
         *leaf_t
@@ -96,6 +97,7 @@ def _test_op(shapes, torch_func, leaf_func, name, timeits=10):
         torch_t,
         _listout_t,
     )).timeit(timeits) * 1000.0 / timeits
+
     b_leaf_ms = timeit.Timer(partial(
         lambda f, t, b: f(*t)[0].mean().backward() if b else f(*t).mean().backward(),
         leaf_func,
